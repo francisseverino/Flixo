ff12aaaa4b3f8a91c7b90bb940491fbdf3b116af
Francis Severino <francis@Franciss-MBP.cable.rcn.com>
2020-11-17 20:40:14 -0500

    Tests WIP

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import request from '../../api/helper';
 import { requests } from '../../api/constants';
 import { MovieData } from '../../types';
 import './Banner.css';
 
 function Banner() {
-  const [movie, setMovie] = useState<MovieData>();
+  const [movie, setMovie] = React.useState<MovieData>();
 
-  useEffect(() => {
+  React.useEffect(() => {
     request(requests.popular).then(response => {
       const randomIndex = Math.floor(Math.random() * response.results.length - 1);
       setMovie(response.results[randomIndex]);
     });
   }, []);
-
-  // useEffect(() => {
-  //   request(`/movie/${movie?.id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
-  //     console.log(response);
-  //   });
-  // }, [movie]);
 
   const truncate = (str: string, n: number) => {
     return str?.length > n ? str.substr(0, n - 1) + '...' : str;
@@ -37,13 +31,14 @@ function Banner() {
     >
       <div className='banner__contents'>
         <h1 className='banner__title'>{movie?.title || movie?.name || movie?.original_name}</h1>
-        <div className='banner__buttons'>
-          <button className='banner__button'>Play</button>
-          <button className='banner__button'>My List</button>
-        </div>
 
         <h1 className='banner__description'>{truncate(movie?.overview || '', 150)}</h1>
+        <div className='banner__buttons'>
+          <button className='banner__button'>Play</button>
+          <button className='banner__button'>More Info</button>
+        </div>
       </div>
+
       <div className='banner--fadeBottom' />
     </header>
   );
--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './Home';
+import { shallow } from 'enzyme';
+//   import { render, screen } from '@testing-library/react';
+import Home from './Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<Home />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('Tets Home page', () => {
+  it('renders without crashing', () => {
+    shallow(<Home />);
+  });
 });
