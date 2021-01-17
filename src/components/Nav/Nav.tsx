b6bd6efadcfd137f93aaf97627b910bd9e299e08
Francis Severino <francisjsg2@gmail.com>
2021-01-17 15:31:51 -0500

    New navbar and montserrat font

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import * as FiIcons from 'react-icons/fi';
-import { SidebarData } from './SidebarData';
 import './Nav.css';
-import { IconContext } from 'react-icons';
 
 function Sidebar() {
-  const [showSidebar, setShowSidebar] = useState<boolean>(false);
-  const [show, handleShow] = useState(false);
+  const [show, handleShow] = React.useState<boolean>(true);
+  const scrollPosition = React.useRef<number>(window.scrollY);
 
-  useEffect(() => {
+  React.useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
     return () => {
@@ -18,43 +16,38 @@ function Sidebar() {
   }, []);
 
   const handleScroll = () => {
-    if (window.scrollY > 100) {
-      handleShow(true);
-    } else handleShow(false);
+    const currentScrollPosition = window.scrollY;
+    handleShow(scrollPosition.current > currentScrollPosition);
+    scrollPosition.current = currentScrollPosition;
   };
 
-  const toggleSidebar = () => setShowSidebar(!showSidebar);
-
   return (
-    <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <div className={`nav ${show && 'nav__black'}`}>
-          <Link to='#' className='sidebar__open'>
-            <FiIcons.FiMenu onClick={toggleSidebar} />
-          </Link>
-          <img className='nav__avatar' src='https://pbs.twimg.com/media/ESHtph2WAAMQAUR.jpg' alt='User Avatar' />
-        </div>
-        <nav className={showSidebar ? 'sidebar__menu active' : 'sidebar__menu'}>
-          <ul className='sidebar__menuItems' onClick={toggleSidebar}>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.className}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-            {/* <li className='sidebar__toggle'>
-              <Link to='#' className='sidebar__open'>
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li> */}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
+    <div className={`nav ${!show && 'nav__up'}`}>
+      <Link to='/' className='nav__logo'>
+        Flixo
+      </Link>
+
+      <nav>
+        <ul className='nav__links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/movies'>Movies</Link>
+          </li>
+          <li>
+            <Link to='/shows'>Shows</Link>
+          </li>
+          <li>
+            <Link to='/people'>People</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Link to='/search' className='nav__button'>
+        <FiIcons.FiSearch className='nav__search' />
+      </Link>
+    </div>
   );
 }
 
